409f0175f49bfbc264b79a83c9194465b3935bc4
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-07 21:50:38 +0800

    add ocr test

--- test1.py
+++ test1.py
@@ -1,6 +1,8 @@
 import easyocr
 reader = easyocr.Reader(['ch_sim','en']) # need to run only once to load model into memory
-result = reader.readtext('WechatIMG7.png')
+filename = 'WechatIMG7.png'
+#result = reader.readtext(filename)
+result = reader.readtext(filename, detail = 0)
 
 print(result)
 
