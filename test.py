376a35143643281f1bf2646e85e55259d94ca3a6
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-08 10:29:50 +0800

    add ocr test

--- test.py
+++ test.py
@@ -3,3 +3,8 @@ reader = easyocr.Reader(['ch_sim','en']) # need to run only once to load model i
 result = reader.readtext('chinese.jpg')
 
 print(result)
+
+# UserWarning: page-1 is image-based, camelot only works on text-based pages. [lattice.py:394]
+import camelot
+tables = camelot.read_pdf('0002514084.pdf')
+tables.export('0002514084.csv', f='csv', compress=False)
